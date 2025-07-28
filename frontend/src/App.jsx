320a0e3e48df21b9baaa12521214220ad765954d
Kshemaahna <63880115+Kshemaahna@users.noreply.github.com>
2025-07-28 15:09:35 -0700

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import UploadForm from './components/UploadForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Realistic Ocean Simulation</h1>
+      <UploadForm />
+    </div>
+  );
+}
+
+export default App;
